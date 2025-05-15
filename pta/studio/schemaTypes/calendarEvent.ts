c411df9dce663185e5ac53f3370b0af1340db208
olegponyat <113192651+olegponyat@users.noreply.github.com>
2025-05-15 01:09:55 -0400

    ts

---
+++ pta/studio/schemaTypes/calendarEvent.ts
@@ -0,0 +1,40 @@
+import { defineType } from 'sanity'
+
+export default defineType({
+  name: 'calendarEvent',
+  title: 'Calendar Event',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [{ type: 'block' }],
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'datetime',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+})
