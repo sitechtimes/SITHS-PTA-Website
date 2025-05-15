fd25dce16cdb01418481bbc1b73b1d36af77cdfd
Bridget <fbridget0404@gmail.com>
2025-05-14 23:59:25 -0400

    Update Landing.vue

--- pta/components/Landing.vue
+++ pta/components/Landing.vue
@@ -7,8 +7,8 @@
         </div>
         <div class="mx-4 md:mx-16 lg:mx-10 flex flex-col justify-center">
             <div id="upcomingEvents">
-                <h3 class="subh flex items-center gap-2 font-bold text-xl mb-3 text-[#4b3a23]">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="w-5 h-5 fill-current">
+                <h3 class="subh flex items-center justify-start gap-2 font-bold text-2xl lg:text-4xl mb-3 text-[#4b3a23]">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="size-5 lg:size-8 fill-current">
                         <path d="M480 32c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9L381.7 53c-48 48-113.1 75-181 75H192 160 64c-35.3 0-64 28.7-64 64v96c0 35.3 28.7 64 64 64l0 128c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32V352l8.7 0c67.9 0 133 27 181 75l43.6 43.6c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V300.4c18.6-8.8 32-32.5 32-60.4s-13.4-51.6-32-60.4V32zm-64 76.7V240 371.3C357.2 317.8 280.5 288 200.7 288H192V192h8.7c79.8 0 156.5-29.8 215.3-83.3z"/>
                     </svg>
                     Upcoming Events
