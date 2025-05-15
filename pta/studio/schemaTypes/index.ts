8489bee13477c277fe0088bc086c9a56eb32d979
olegponyat <113192651+olegponyat@users.noreply.github.com>
2025-05-15 00:22:25 -0400

    Update index.ts

--- pta/studio/schemaTypes/index.ts
+++ pta/studio/schemaTypes/index.ts
@@ -1,19 +1,13 @@
 import staff from './staff'
 import homePageContent from './homePageContent'
 import gallery from './gallery'
-<<<<<<< HEAD
 import resources from './resources'
 import donationPageContent from './donationPageContent'
-=======
->>>>>>> 85b6b7d (Revert "uuhuuhhhghgghhghhg")
 
 export const schemaTypes = [
     staff,
     homePageContent,
     donationPageContent,
     gallery,
-<<<<<<< HEAD
     resources
-=======
->>>>>>> 85b6b7d (Revert "uuhuuhhhghgghhghhg")
 ]
