1834e77371ba9ce5ab973eac250fb24a433b7d0d
Oleg Ponyatyshyn <113192651+olegponyat@users.noreply.github.com>
2025-05-21 11:03:12 -0400

    Update JoinUs.vue

--- pta/components/JoinUs.vue
+++ pta/components/JoinUs.vue
@@ -1,18 +1,13 @@
 <template>
     <div>
         <ul>
-            <li v-for="(item, index) in joinUsContent" :key="index">
-                <!-- Access individual children and their text -->
-                <p v-for="(child, childIndex) in item.children" :key="childIndex">
-                    {{ child.text }}
-                </p>
+            <li v-for="(item, index) in joinUsContent.children[0].text" :key="index">
+                {{ item }}
             </li>
         </ul>
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
 const { joinUsContent } = fetchTextData();
-console.log(joinUsContent);
 </script>
