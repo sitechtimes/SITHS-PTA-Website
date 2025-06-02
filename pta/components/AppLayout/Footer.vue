94cce9f9d9c7b1ee6a5547a92097b4cd5f232021
Oleg Ponyatyshyn <113192651+olegponyat@users.noreply.github.com>
2025-06-02 11:02:59 -0400

    THIS DOES NOT WORK DO NOT MERGE

--- pta/components/AppLayout/Footer.vue
+++ pta/components/AppLayout/Footer.vue
@@ -11,16 +11,16 @@
         </div>
         <p class="text-xs mt-2">SITHS PTA @2024</p>
       </div>
-      <div id="secondSection" class="flex-1 w-full lg:w-auto mx-4 mb-8 lg:mb-0">
-        <div v-for="info in websiteInformation" id="email" class="flex my-4">
-            <img :src="info.imageUrl" :alt="info.title" class="w-8">
-          <a :href="info.link" class="ml-3">{{ info.title }}</a>
+      <div class="flex-1 lg:w-auto mb-8 lg:mb-90 flex flex-col flex-wrap justify-center max-w-96 max-h-80">
+        <div v-for="info in websiteInformation" id="email" class="flex flex-col items-center my-1 sm:w-1/3 max-w-64">
+          <img :src="info.imageUrl" :alt="info.title" class="w-8 mb-2">
+          <a :href="info.link" class="text-center">{{ info.title }}</a>
         </div>
       </div>
       <div id="thirdSection" class="flex-1 w-full lg:w-auto mx-4">
         <h2 class="font-bold">About Us</h2>
         <p class="text-sm mb-8">
-          {{ websiteInformation.about }}
+          {{ homePageContent.about }}
         </p>
         <NuxtLink to="/Donate" class="bg-dark-brown text-white p-3 text-sm rounded-lg">Donate Now!</NuxtLink>
       </div>
@@ -29,8 +29,7 @@
 </template>
 
 <script setup>
-const { websiteInformation } = await useWebsiteData()
-console.log(websiteInformation.value)
+const { websiteInformation, homePageContent } = await useWebsiteData()
 </script>
 
 <style scoped>
