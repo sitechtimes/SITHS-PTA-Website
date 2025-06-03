9972dec6d13a95346af87bd86b3655117a7214d0
olegponyat <113192651+olegponyat@users.noreply.github.com>
2025-06-02 22:36:25 -0400

    quick fix

--- pta/studio/schemaTypes/gallery.ts
+++ pta/studio/schemaTypes/gallery.ts
@@ -1,27 +1,27 @@
-import { defineType } from 'sanity'
+import { defineType, defineField } from 'sanity'
 
 export default defineType({
   name: 'galleryImage',
   title: 'Gallery Image',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'image',
       title: 'Image',
       type: 'image',
       options: { hotspot: true },
       validation: rule => rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'alt',
       title: 'Alt Text',
       type: 'string',
       validation: rule => rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'order',
       title: 'Order',
       type: 'number',
-    },
+    }),
   ],
 })
