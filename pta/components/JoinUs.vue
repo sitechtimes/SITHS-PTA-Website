d2a9bf5335b740b298a436782c26b2fcebdd1d93
Oleg Ponyatyshyn <113192651+olegponyat@users.noreply.github.com>
2025-06-04 11:02:33 -0400

    alomst

--- pta/components/JoinUs.vue
+++ pta/components/JoinUs.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
-        <PortableText :value="joinUsContent" />
+        <PortableText 
+            :value="joinUsContent" :components="myPortableTextComponents"/>
     </div>
 </template>
 
 <script setup>
 import { PortableText } from '@portabletext/vue'
 const { joinUsContent } = await fetchTextData();
+
+const myPortableTextComponents = {
+    marks: {
+        link: ({ value }, { slots }) => {
+            const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined;
+            return h('a', { href: value.href, rel, style: 'color: blue; text-decoration: underline;' }, slots.default?.());
+        },
+    },
+};
 console.log(joinUsContent.value?.[1]?.children?.[0].text)
 </script>
