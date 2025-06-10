447997f95e7bd5fc2cc0719602e10160b721eae3
olegponyat <113192651+olegponyat@users.noreply.github.com>
2025-06-09 21:54:46 -0400

    Update Footer.vue

--- pta/components/AppLayout/Footer.vue
+++ pta/components/AppLayout/Footer.vue
@@ -13,19 +13,19 @@
       </div>
       <div class="flex-1 lg:w-auto mb-8 flex flex-col justify-center">
         <div class="flex flex-col flex-wrap content-start justify-start max-h-50">
-            <div v-for="info in websiteInformation" :key="info.title">
-              <div v-if="info.title" class="flex items-center py-1 text-sm">
-                <img :src="info.imageUrl" alt="" class="w-8 h-8 m-2 shrink-0">
-                <a v-if="info.link" :href="info.link">{{ info.title }}</a>
-                <span v-else>{{ info.title }}</span>
-              </div>
+          <div v-for="info in websiteInformation" :key="info.title">
+            <div v-if="info.title" class="flex items-center py-1 text-sm">
+              <img :src="info.imageUrl" alt="" class="w-8 h-8 m-2 shrink-0">
+              <a v-if="info.link" :href="info.link">{{ info.title }}</a>
+              <span v-else>{{ info.title }}</span>
             </div>
+          </div>
         </div>
         <div class="flex mt-4 space-x-3">
           <div v-for="info in websiteInformation" :key="info.title">
             <div v-if="!info.title" class="p-1">
               <a :href="info.link" target="_blank" rel="noopener noreferrer" :aria-label="info.link || 'External Link'">
-                <img :src="info.imageUrl" alt="" class="w-8">
+                <img :src="info.imageUrl" class="w-8" alt="">
               </a>
             </div>
           </div>
@@ -33,9 +33,9 @@
       </div>
       <div id="thirdSection" class="flex-1 w-full lg:w-auto mx-4">
         <h2 class="font-bold">About Us</h2>
-        <p class="text-sm mb-8">
+        <div class="text-sm mb-8">
           <PortableText v-if="aboutUsData" :value="aboutUsData.aboutUsContent[0]" />
-        </p>
+        </div>
         <NuxtLink to="/Donate" class="bg-dark-brown text-white p-3 text-sm rounded-lg">Donate Now!</NuxtLink>
       </div>
     </div>
@@ -45,7 +45,7 @@
 <script setup type="ts">
 import { PortableText } from '@portabletext/vue'
 const { websiteInformation } = await useWebsiteData()
-const { aboutUsData } = await useAsyncData('footerAboutUs', async () => {
+const { data: aboutUsData } = await useAsyncData('footerAboutUs', async () => {
   const { aboutUsContent } = await fetchTextData();
   return { aboutUsContent };
 });
