a70e65cffae257aeec121ecc72dc3843cfb0114b
Oleg Ponyatyshyn <113192651+olegponyat@users.noreply.github.com>
2025-09-15 07:29:55 -0700

    oopsies

--- pta/studio/schemaTypes/index.ts
+++ pta/studio/schemaTypes/index.ts
@@ -5,13 +5,17 @@ import resources from './resources'
 import donationPageContent from './donationPageContent'
 import calendarEvent from './calendarEvent'
 import joinUsPageContent from './joinUs'
+import websiteInformation from './websiteInformation'
+import slt from './slt'
 
 export const schemaTypes = [
     staff,
+    slt,
     homePageContent,
     donationPageContent,
     gallery,
     resources,
     calendarEvent,
-    joinUsPageContent
+    joinUsPageContent,
+    websiteInformation
 ]
