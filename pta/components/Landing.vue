06a6b35db4982366d7f877b91f6b359d3bf508bc
olegponyat <113192651+olegponyat@users.noreply.github.com>
2025-09-18 20:40:31 -0400

    MORE CSS

--- pta/components/Landing.vue
+++ pta/components/Landing.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid lg:grid-cols-2 my-16 mx-2">
+  <div class="grid lg:grid-cols-2 my-16">
     <div class="mx-4 md:mx-9">
       <h2 class="font-normal lg:text-2xl md:xl" id="PTA">
         Parent Teacher Association @
